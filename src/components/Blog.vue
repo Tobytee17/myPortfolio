b8ec4b42a426ccb69e742036a1dce4b5d8e0152a
Toby <Tobytee17@gmail.com>
2017-07-10 12:03:03 +1200

    Added blog.vue
    Git somehow left out the blog.vue file. I have added it in this commit.

---
+++ src/components/Blog.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="blog">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12" v-for="post in posts">
+        <div class="well">
+          <div class="page-header">
+            <h1><router-link :to="post.link" v-text="post.title"></router-link></h1><small v-text="post.date"></small>
+          </div>
+          <p v-text="post.summary"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'blog',
+  data () {
+    return {
+        posts: [
+          {title: "TestPost1", date: "10/07/2017", summary: "Here is the content of this article, I am trying to make it a little bit longer so that it can look like a real post", link: ""},
+          {title: "TestPost2", date: "10/07/2017", summary: "Here is the content of this article, I am trying to make it a little bit longer so that it can look like a real post", link: ""},
+          {title: "TestPost3", date: "10/07/2017", summary: "Here is the content of this article, I am trying to make it a little bit longer so that it can look like a real post", link: ""}
+        ]
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.page-header {
+  text-align: left;
+  margin: 0px 0px 0px 0px;
+}
+
+small {
+  font-style: italic;
+}
+
+.well {
+  margin: 20px 10% 20px 10%;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
